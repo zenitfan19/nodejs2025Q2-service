a44955bbd5039a57f14a8efcbca34013ecdabb07
Andrey Lavrov <andrey_lavrov@epam.com>
2025-06-02 14:35:38 +0500

    feat: receive port from dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig();
 
 async function bootstrap() {
+  const port = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  await app.listen(port);
 }
 bootstrap();
