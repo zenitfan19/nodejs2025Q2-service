4a387cccee7ae981c8a2dcf3257377c5436dfc17
Andrey Lavrov <andrey_lavrov@epam.com>
2025-06-02 17:47:30 +0500

    feat: add uuid validator

---
+++ src/utils/uuid.util.ts
@@ -0,0 +1,8 @@
+import { BadRequestException } from '@nestjs/common';
+
+export function validateUuid(id: string): void {
+  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  if (!uuidRegex.test(id)) {
+    throw new BadRequestException('Invalid UUID format');
+  }
+}
