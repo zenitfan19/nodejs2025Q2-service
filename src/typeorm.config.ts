cf25a1ac32d0da1120776be6b034a5607150e67a
Andrey Lavrov <fanton96mailru@gmail.com>
2025-06-09 16:51:25 +0500

    feat: add docker compose

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,3 +1,4 @@
+import { config as dotenvConfig } from 'dotenv';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
 import { Artist } from './artist/artist.entity';
@@ -5,13 +6,15 @@ import { Album } from './album/album.entity';
 import { Track } from './track/track.entity';
 import { Favorites } from './favorites/favorites.entity';
 
+dotenvConfig();
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'home_library',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  database: process.env.POSTGRES_DB || 'home_library',
   entities: [User, Artist, Album, Track, Favorites],
   synchronize: true,
 };
