6b2d406c7eddab9d145dc4a5a06c2b9ad46f89e8
Andrey Lavrov <fanton96mailru@gmail.com>
2025-06-09 15:17:01 +0500

    feat: update favs module to support typeorm

--- src/favorites/favorites.entity.ts
+++ src/favorites/favorites.entity.ts
@@ -1,11 +1,45 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
 import { Artist } from '../artist/artist.entity';
 import { Album } from '../album/album.entity';
 import { Track } from '../track/track.entity';
 
-export interface Favorites {
-  artists: string[];
-  albums: string[];
-  tracks: string[];
+@Entity('favorites')
+export class Favorites {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  userId: string;
+
+  @ManyToMany(() => Artist)
+  @JoinTable({
+    name: 'favorite_artists',
+    joinColumn: { name: 'favoritesId', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'artistId', referencedColumnName: 'id' },
+  })
+  artists: Artist[];
+
+  @ManyToMany(() => Album)
+  @JoinTable({
+    name: 'favorite_albums',
+    joinColumn: { name: 'favoritesId', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'albumId', referencedColumnName: 'id' },
+  })
+  albums: Album[];
+
+  @ManyToMany(() => Track)
+  @JoinTable({
+    name: 'favorite_tracks',
+    joinColumn: { name: 'favoritesId', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'trackId', referencedColumnName: 'id' },
+  })
+  tracks: Track[];
 }
 
 export interface FavoritesResponse {
--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FavoritesController } from './favorites.controller';
 import { FavoritesService } from './favorites.service';
 import { ArtistModule } from '../artist/artist.module';
 import { AlbumModule } from '../album/album.module';
 import { TrackModule } from '../track/track.module';
+import { Favorites } from './favorites.entity';
 
 @Module({
-  imports: [ArtistModule, AlbumModule, TrackModule],
+  imports: [
+    TypeOrmModule.forFeature([Favorites]),
+    ArtistModule,
+    AlbumModule,
+    TrackModule,
+  ],
   controllers: [FavoritesController],
   providers: [FavoritesService],
   exports: [FavoritesService],
